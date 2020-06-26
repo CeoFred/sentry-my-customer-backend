3d4700d76b5edd200e506ec74cdcfbdacf4835aa
ceofred <johnsonmessilo19@gmail.com>
2020-06-26 11:22:24 +0100

    performance improvement

--- controllers/register_controler.js
+++ controllers/register_controler.js
@@ -9,33 +9,11 @@ module.exports.registerUser = async (req, res, next) => {
     try {
 		const { email, first_name, is_active, last_name, password, phone_number, user_role } = req.body;
 
-    //  Create a Token that will be passed as the "api_token"
-    const apiToken = await jwt.sign({
-        phone_number: phone_number,
-        email: email,
-        user_role: user_role,
-    }, process.env.JWT_KEY, {
-        expiresIn: "1d",
-    });
 
-
-    //  Get instance of the
-    const user = new UserModel({
-        phone_number: phone_number,
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        is_active: is_active,
-        password: password,
-        api_token: apiToken,
-        user_role: user_role,
-    });
-    //  Encrypt the Password
-    user.password = await bCrypt.hash(user.password, 12);
 
 
     //  Check if User PhoneNumber and Email already exist.
-    await UserModel.findOne({ phone_number: user.phone_number, email: user.email })
+    await UserModel.findOne({ phone_number, email })
         .then((existingUser) => {
             if(existingUser) {
                 //  This means the user exists.
@@ -44,22 +22,36 @@ module.exports.registerUser = async (req, res, next) => {
                 });
             }
             else {
+                    //  Create a Token that will be passed as the "api_token"
+    const apiToken = await jwt.sign({
+        phone_number: phone_number,
+        email: email,
+        user_role: user_role,
+    }, process.env.JWT_KEY, {
+        expiresIn: "1d",
+    });
+
+        //  Get instance of the
+        const user = new UserModel({
+            phone_number: phone_number,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            is_active: is_active,
+            password: password,
+            api_token: apiToken,
+            user_role: user_role,
+        });
+        //  Encrypt the Password
+        user.password = await bCrypt.hash(user.password, 12);
+    
+
                 //  Save to the DB.
                 user.save()
                     .then((result) => {
                         res.status(201).json({
                             Message: "User registered successfully...",
-                            User: {
-                                _id: result._id,
-                                phone_number: result.phone_number, 
-                                first_name: result.first_name, 
-                                last_name: result.last_name, 
-                                email: result.email, 
-                                is_active: result.is_active, 
-                                password: result.password, 
-                                api_token: result.api_token, 
-                                user_role: result.user_role,
-                            },
+                            User: {...results},
                         });
 
                         //  TODO Redirect to the OTP Activation Page.
